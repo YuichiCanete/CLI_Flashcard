5574db9ac63b127df87c045dc61adf3d7da6b66b
YuichiCanete <ycanete_220000000743@uic.edu.ph>
2024-09-14 12:51:53 +0800

    global provider comments for riverpod explanation

--- lib/global_providers.dart
+++ lib/global_providers.dart
@@ -3,14 +3,23 @@ import 'package:riverpod/riverpod.dart';
 import './services/card_editor.dart';
 import './services/card_list.dart';
 
+// Easy way to do dependency injection
+// And manage global instances of cardList, cardEditor and cardQuiz
+
 final cardListServiceProvider = Provider((ref) {
+  // maintains list of flashcards
   return CardList();
 });
 
+
 final cardEditorServiceProvider = Provider((ref) {
+  // depends of cardList
+  // modifies the contents of cardList
   return CardEditor(ref);
 });
 
 final cardQuizServiceProvider = Provider((ref) {
+  // depends of cardList
+  // allow user to take quiz based on stored data  in cardList
   return CardQuiz(ref);
 });
